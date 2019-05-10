427470a226324c2eebd7374fbba02387d380c19d
GIPC <anderson.araf@gmail.com>
2019-05-10 12:17:14 -0500

    Class Anime

--- model/Modelanime.php
+++ model/Modelanime.php
@@ -7,31 +7,37 @@ class Anime{
     public function getId(){
         return $this.id;
     }
+
     public function setId($id){
-        $this.id = $id;
+        $this.$id = $id;
     }
 
     public function getNome(){
         return $this.nome;
     }
     public function setNome($nome){
-        $this.nome = $nome;
+        $this.$nome = $nome;
     }
 
     public function getDescricao(){
         return $this.descricao;
     }
     public function setDescricao($descricao){
-        $this.descricao = $descricao;
+        $this.$descricao = $descricao;
     }
 
     public function adcionarAnime(){
-        $sql = "INSET INTO anime(nome, descricao) VALUES(?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(1, getNome());
-        $stmt->bindParam(2, getDescricao());
-        $stmt->execute();
+        try {
+            include "conexao.php";
+            $sql = "INSET INTO anime(nome, descricao) VALUES(?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(1, getNome());
+            $stmt->bindParam(2, getDescricao());
+            $stmt->execute();
+        } catch (PDOException $e) {
+            $e->getMessage();
+        }
+        
     }
 }
-
 ?>
