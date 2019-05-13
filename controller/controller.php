a9211c896ea3f75c2672e96c8c29f63eb3b8b68d
GIPC <anderson.araf@gmail.com>
2019-05-13 11:24:57 -0500

    Controle para adicionar Video

--- controller/controller.php
+++ controller/controller.php
@@ -5,6 +5,7 @@ $req = $_GET['req'];
 #INCLUIR A CLASSE USUÁRIO
 include "../model/usuario.php";
 
+
 #INCLUIR A CONEXÃO
 require "../derivados/conexao.php";
 
@@ -22,14 +23,52 @@ if( $req == "loginAdmin" ){
     #CASO O USUÁRIO EXISTA DEVOLVER A VIEW administracao.php
     if($resp != 0){
         
-        return  header('location: ../view/adiminitracao.php');
+        return  header('location: ../view/administracao.php');
     }
     #CASO O USUARIO NÂO EXISTA DEVOLVER A VIEW login.php
     return  header('location: ../view/loginAdmin.php');
 }
 
-if($req == ){
+if($req == "adicionarVideo"){
+    #INCLUIR OBJETOS
+    include "../model/anime.php";
+    include "../model/videoAnime.php";
 
+    #GET PARAMETRS FORM
+    $nome = $_POST['inputNome'];
+    $desc = $_POST['txtDesc'];
+    $url = $_POST['inputURL'];
+
+    #CRIAR O OBJETO ANIME E VIDEOANIME
+    $anime = new Anime();
+    #SET VALORES ANIME
+    $anime->setNome($nome);
+    $anime->setDescricao($desc);
+    #INSERT VALUES DATABASE
+    if($anime->inserirAnime($conn) >= 1){
+        #PEGAR O RETURN DO SELECTID
+        $id = $anime->selectId($conn);
+        #SETA O RETORNO NO ID
+        $anime->setId($id);
+        #SELECT ID, PARA ENVIAR O VALOR DO OBEJTO COMPLETO A funcão EnviarVideo
+
+        if($anime->getId() >= 0){
+            #CREATE OBJECT VIDEOANIME
+            $videoAnime = new videoAnime;
+            #SET VALUES VIDEOANIMES
+            $videoAnime->setURL($url);
+            
+           #SETAR URL
+           try{
+                $videoAnime->inserirURL($anime->getId(), $conn);
+                return header("Location: ../view/index.php");
+           }catch(Exception $e){
+                echo $e->getMessage();
+           }
+        }
+    }
+    #ERRO NA HORA DE INSERIR É REDIRECIONADO
+    return header("Location: ../view/administracao.php");
 }
 
 
