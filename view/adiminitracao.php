ac661b2a31d27d28e50e4c3109ab2630cd7c4674
GIPC <anderson.araf@gmail.com>
2019-05-13 09:17:24 -0500

    Áreas adminsitrativas - login/Portal

---
+++ view/adiminitracao.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php
+        require "../derivados/session.php";
+    ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Área Administrativa</title>
+</head>
+<body>
+    
+</body>
+</html>
